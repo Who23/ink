89302feba4867ec347b830a01a1ad8c811af4178
Who23 <40632266+Who23@users.noreply.github.com>
2020-06-17 16:45:30 -0400

    Fix Formatting
    Used clippy and rustfmt to reformat code

--- src/diff.rs
+++ src/diff.rs
@@ -1,8 +1,8 @@
 //! Tools for creating diffs, done through the `Diff` struct
-use std::io::{self, BufRead, Write, BufWriter, BufReader};
-use std::fs::{self, File};
-use std::path::Path;
 use std::error::Error;
+use std::fs::{self, File};
+use std::io::{self, BufRead, BufReader, BufWriter, Write};
+use std::path::Path;
 
 /// The type of edit - Insertion or Deletion
 #[derive(PartialEq)]
@@ -12,14 +12,14 @@ enum EditOp {
 }
 
 /// One section of a diff which involves adding or removing one
-/// or more lines. 
+/// or more lines.
 ///
 /// This should only be created via the `Diff` struct
 pub struct Edit {
     operation: EditOp,
     line_start: usize,
     line_end: usize,
-    content: String
+    content: String,
 }
 
 impl Edit {
@@ -28,19 +28,22 @@ impl Edit {
     /// though this is not an 'ed' compatible edit script
     fn to_edit_script(&self) -> String {
         let op = match self.operation {
-            EditOp::Insert  => "a",
-            EditOp::Delete  => "d"
+            EditOp::Insert => "a",
+            EditOp::Delete => "d",
         };
 
-        format!("{},{},{}\n{}", self.line_start, self.line_end, op, self.content)
+        format!(
+            "{},{},{}\n{}",
+            self.line_start, self.line_end, op, self.content
+        )
     }
 }
 
-/// Struct that holds the diff of two files. 
+/// Struct that holds the diff of two files.
 ///
 /// Constructs and holds a sequence of `Edit`.
 pub struct Diff {
-    pub edits: Vec<Edit>
+    pub edits: Vec<Edit>,
 }
 
 impl Diff {
@@ -48,16 +51,14 @@ impl Diff {
     pub fn from(a: Vec<String>, b: Vec<String>) -> Diff {
         let trace = explore_paths(&a, &b);
         let path = find_path(trace, a.len(), b.len());
-        let edits = create_edits(path, a, b);
-        
-        Diff {
-            edits
-        }
+        let edits = create_edits(path, &a, &b);
+
+        Diff { edits }
     }
 
     /// Deserialize an edit script to create a diff
     pub fn from_edit_script(edit_script: Vec<String>) -> Result<Diff, Box<dyn Error>> {
-        let mut edits = Vec::new(); 
+        let mut edits = Vec::new();
 
         // store what will be in the edit through the loop
         let mut line_start = 0;
@@ -87,13 +88,13 @@ impl Diff {
             }
 
             // get the starting line #, ending line #, & type of edit
-            let components: Vec<&str> = line.split(",").collect();
+            let components: Vec<&str> = line.split(',').collect();
             line_start = components[0].parse::<usize>()?;
             line_end = components[1].parse::<usize>()?;
             operation = match components[2] {
                 "a" => Ok(EditOp::Insert),
                 "d" => Ok(EditOp::Delete),
-                 _  => Err("invalid edit script")
+                _ => Err("invalid edit script"),
             }?;
 
             // find how many lines of content there will be
@@ -105,44 +106,42 @@ impl Diff {
             operation,
             line_start,
             line_end,
-            content: content.clone(),
+            content,
         });
 
-        Ok(Diff {
-            edits
-        })
+        Ok(Diff { edits })
     }
 
     /// Serialize an 'edit script' for the diff.
     /// The changes in the edit script are thought to happen simultaneously.
     pub fn edit_script(&self) -> String {
-        self.edits.iter()
-                  .map(|e| e.to_edit_script())
-                  .collect::<Vec<String>>()
-                  .join("")
+        self.edits
+            .iter()
+            .map(|e| e.to_edit_script())
+            .collect::<Vec<String>>()
+            .join("")
     }
 
-
     /// Applies a series of edits to a file
-    /// Goes line by line through the file to add edits in a tmp file, 
+    /// Goes line by line through the file to add edits in a tmp file,
     /// then overwriting the normal file with the tmp file.
-    fn apply_edits(edits: &Vec<Edit>, file_path: &Path) -> io::Result<()> {
+    fn apply_edits(edits: &[Edit], file_path: &Path) -> io::Result<()> {
         let tmp_path = file_path.with_extension(".tmp");
 
         // check if there are any edits
-        if edits.len() == 0 {
-            return Ok(())
+        if edits.is_empty() {
+            return Ok(());
         }
 
         // open up the file to read and a tmp file to write to
         let file = BufReader::new(File::open(file_path)?);
         let mut tmp = BufWriter::new(File::create(&tmp_path)?);
 
-        let mut lines_to_delete = 0;    // if deleting lines, how many are left to delete
-        let mut current_edit_index = 0;   // current edit we're on
-        let mut current_edit = &edits[current_edit_index];   
-        let mut last_edit = &edits[current_edit_index];   // the previous edit
-        let mut next_edit = &edits[current_edit_index + 1];    // the next edit
+        let mut lines_to_delete = 0; // if deleting lines, how many are left to delete
+        let mut current_edit_index = 0; // current edit we're on
+        let mut current_edit: &Edit;
+        let mut last_edit = &edits[current_edit_index]; // the previous edit
+        let mut next_edit: &Edit; // the next edit
 
         for (line_number, line) in file.lines().enumerate() {
             let line = line?;
@@ -161,7 +160,7 @@ impl Diff {
                 match current_edit.operation {
                     EditOp::Insert => {
                         // write the inserted lines into the tmp file
-                        tmp.write(current_edit.content.as_bytes())?;
+                        tmp.write_all(current_edit.content.as_bytes())?;
 
                         // decides whether to write the current line of the normal file into the tmp file
                         // if this is not the last edit & the next edit would delete this line
@@ -169,16 +168,17 @@ impl Diff {
                         if current_edit_index + 1 != edits.len() {
                             next_edit = &edits[current_edit_index + 1];
 
-                            if !(next_edit.operation == EditOp::Delete && current_edit.line_end + 1 == next_edit.line_start) {
-                                tmp.write((line + "\n").as_bytes())?;
+                            if !(next_edit.operation == EditOp::Delete
+                                && current_edit.line_end + 1 == next_edit.line_start)
+                            {
+                                tmp.write_all((line + "\n").as_bytes())?;
                             }
-
                         } else {
-                            tmp.write((line + "\n").as_bytes())?;
+                            tmp.write_all((line + "\n").as_bytes())?;
                         }
 
                         last_edit = current_edit;
-                    },
+                    }
                     EditOp::Delete => {
                         // how many lines we should be deleting? if the end == the start, then we
                         // only delete this line (sets to 0)
@@ -187,8 +187,10 @@ impl Diff {
                         // checks if we write the current line into the tmp file
                         // checks if the last edit was an insert that already deleted the line that
                         // we needed to delete, so we should write this next line into the tmp file
-                        if last_edit.operation == EditOp::Insert && last_edit.line_end + 1 == current_edit.line_start {
-                            tmp.write((line + "\n").as_bytes())?;
+                        if last_edit.operation == EditOp::Insert
+                            && last_edit.line_end + 1 == current_edit.line_start
+                        {
+                            tmp.write_all((line + "\n").as_bytes())?;
 
                             if lines_to_delete > 0 {
                                 lines_to_delete -= 1;
@@ -198,10 +200,11 @@ impl Diff {
                         last_edit = current_edit;
                     }
                 }
+
                 current_edit_index += 1;
             } else {
                 // if there wasn't an edit, write the current line
-                tmp.write((line + "\n").as_bytes())?;
+                tmp.write_all((line + "\n").as_bytes())?;
             }
         }
 
@@ -210,13 +213,12 @@ impl Diff {
         if current_edit_index == edits.len() - 1 {
             current_edit = &edits[current_edit_index];
             if current_edit.operation == EditOp::Insert {
-                tmp.write(current_edit.content.as_bytes())?;
+                tmp.write_all(current_edit.content.as_bytes())?;
             } else {
                 panic!("the last one is a delete??");
             }
-            
         } else if current_edit_index != edits.len() {
-            panic!("&edits left after??");
+            panic!("edits left after??");
         }
 
         // drop the writer to the tmp file
@@ -262,9 +264,9 @@ impl Diff {
                         operation: EditOp::Delete,
                         line_start,
                         line_end: num_lines + line_start - 1,
-                        content: edit.content.clone()
+                        content: edit.content.clone(),
                     }
-                },
+                }
                 // same as above for the delete
                 EditOp::Delete => {
                     let num_lines = edit.content.lines().count();
@@ -283,7 +285,7 @@ impl Diff {
                         operation: EditOp::Insert,
                         line_start,
                         line_end: line_start + num_lines - 1,
-                        content: edit.content.clone()
+                        content: edit.content.clone(),
                     }
                 }
             };
@@ -297,7 +299,7 @@ impl Diff {
 
 /// Creates a vector of `Edit`s given a path through the edit graph
 /// Final part of the Myers' Diff Algorithm
-fn create_edits(path: Vec<(usize, usize)>, a: Vec<String>, b: Vec<String>) -> Vec<Edit> {
+fn create_edits(path: Vec<(usize, usize)>, a: &[String], b: &[String]) -> Vec<Edit> {
     let mut diff: Vec<Edit> = Vec::new();
 
     let mut x = 0;
@@ -311,26 +313,26 @@ fn create_edits(path: Vec<(usize, usize)>, a: Vec<String>, b: Vec<String>) -> Ve
         // Diagonal move means same between files
         let edit_type = if x == prev_x {
             Some(EditOp::Insert)
-
         } else if y == prev_y {
             Some(EditOp::Delete)
-
-        } else { None };
+        } else {
+            None
+        };
 
         if let Some(edit_type) = edit_type {
             // get what we are inserting/deleting and where
             // insert --> coming from the 2nd string, opposite for delete
             // the line number is always from the first file
             let (line_idx, lines) = match edit_type {
-                EditOp::Insert => { (x, &b[y]) },
-                EditOp::Delete => { (x, &a[x]) }
+                EditOp::Insert => (x, &b[y]),
+                EditOp::Delete => (x, &a[x]),
             };
 
             // If the last edit was of the same type, expand that edit
             // instead of creaing a new one. Otherwise add the new edit to the diff
             if let Some(edit) = diff.last_mut() {
                 if edit.operation == edit_type && (edit.line_end + 1) >= line_idx {
-                    edit.line_end = edit.line_end + 1;
+                    edit.line_end += 1;
                     edit.content.push_str(lines);
                     edit.content.push_str("\n");
                 } else {
@@ -338,7 +340,7 @@ fn create_edits(path: Vec<(usize, usize)>, a: Vec<String>, b: Vec<String>) -> Ve
                         operation: edit_type,
                         line_start: line_idx,
                         line_end: line_idx,
-                        content: (*lines).clone() + "\n"
+                        content: (*lines).clone() + "\n",
                     });
                 }
             } else {
@@ -346,7 +348,7 @@ fn create_edits(path: Vec<(usize, usize)>, a: Vec<String>, b: Vec<String>) -> Ve
                     operation: edit_type,
                     line_start: line_idx,
                     line_end: line_idx,
-                    content: (*lines).clone() + "\n"
+                    content: (*lines).clone() + "\n",
                 });
             }
         }
@@ -386,8 +388,7 @@ fn find_path(trace: Vec<Vec<usize>>, a_len: usize, b_len: usize) -> Vec<(usize, 
         // same logic as traversing it
         let prev_k = if (k == -d) || (k != d && v[index_k - 1] < v[index_k + 1]) {
             k + 1
-        }
-        else {
+        } else {
             k - 1
         };
 
@@ -424,7 +425,9 @@ fn find_path(trace: Vec<Vec<usize>>, a_len: usize, b_len: usize) -> Vec<(usize, 
 /// Explores possible paths in the edit graph, to find a possible SES,
 /// Filling in the trace along the way
 /// First part of the Myers' Diff Algorithm
-fn explore_paths(a: &Vec<String>, b: &Vec<String>) -> Vec<Vec<usize>> {
+/// Single char names because it matches the paper
+#[allow(clippy::many_single_char_names)]
+fn explore_paths(a: &[String], b: &[String]) -> Vec<Vec<usize>> {
     let (n, m) = (a.len(), b.len());
     let max = n + m;
     let mut v = vec![0; 2 * max + 1];
@@ -436,14 +439,13 @@ fn explore_paths(a: &Vec<String>, b: &Vec<String>) -> Vec<Vec<usize>> {
     for d in 0..=max {
         // usually k would be iterating from -d <-> d. But isize is a pain here,
         // so it maps to 0 <-> 2d
-        for k in (0..=(2*d)).step_by(2) {
+        for k in (0..=(2 * d)).step_by(2) {
             // k is used as an index in the algorithm
             // but since k is mapped to 0 <-> 2d it's a problem, so this is a unique index
             let index_k = (max - d) + k;
-            let mut x = if (k == 0) || (k != 2*d && v[index_k - 1] < v[index_k + 1]) {
+            let mut x = if (k == 0) || (k != 2 * d && v[index_k - 1] < v[index_k + 1]) {
                 v[index_k + 1]
-            }
-            else {
+            } else {
                 v[index_k - 1] + 1
             };
 
@@ -462,10 +464,11 @@ fn explore_paths(a: &Vec<String>, b: &Vec<String>) -> Vec<Vec<usize>> {
 
             // we have reached the end point!
             if x >= n && y >= m {
-                t.push(v.clone());
+                t.push(v);
                 return t;
             }
         }
+
         t.push(v.clone());
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::fs::{File};
-use std::io::{BufReader, BufRead};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::path::Path;
 
 use ink::diff::Diff;
