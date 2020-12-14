dcdda944d8f28b9d9cc12c2dc672209c9731c89d
Who23 <contactwho23@gmail.com>
2020-12-14 10:34:05 -0500

    Turn `Node` into a trait

--- src/graph.rs
+++ src/graph.rs
@@ -5,25 +5,26 @@ use std::collections::HashMap;
 type NodeID = usize;
 
 /// The Graph struct holds all the relevant information of the graph.
-/// Contains a list of Nodes of type T, as well as an internal ID counter.
+/// Contains a HashMap of Nodes, as well as an internal ID counter.
 /// This should be created with `Graph::new()` or `Default::default()`
-pub struct Graph<T> {
-    pub nodes: HashMap<NodeID, Node<T>>,
+pub struct Graph<N: Node> {
+    pub nodes: HashMap<NodeID, N>,
     id_counter: NodeID,
 }
 
-/// A graph node of type T
-/// Holds T, an ID, the nodes it points to and the nodes that point to it.
-/// A node may point to itself.
-/// This should never be created by the user, only by the `Graph` struct.
-pub struct Node<T> {
-    obj: T,
-    id: NodeID,
-    pointing_to: Vec<NodeID>,
-    pointed_to: Vec<NodeID>,
+/// A graph node
+/// Must store/return an ID, a vec of nodes it is pointing to, 
+/// and a vec of nodes pointing to it.
+pub trait Node {
+    type Inner;
+
+    fn new(obj: Self::Inner, id: NodeID) -> Self;
+    fn pointing_to(&mut self) -> &mut Vec<NodeID>;
+    fn pointed_to(&mut self) -> &mut Vec<NodeID>;
+    fn id(&self) -> NodeID;
 }
 
-impl<T> Graph<T> {
+impl<N: Node> Graph<N> {
     /// Create a new graph
     pub fn new() -> Self {
         Graph {
@@ -32,14 +33,10 @@ impl<T> Graph<T> {
         }
     }
 
-    /// Add a node to the graph, given T. Returns the ID of the created node.
-    pub fn add_node(&mut self, obj: T) -> NodeID {
-        let node = Node {
-            obj,
-            id: self.id_counter,
-            pointing_to: vec![],
-            pointed_to: vec![],
-        };
+    /// Add a node to the graph, given the inner object.
+    /// Returns the ID of the created node.
+    pub fn add_node(&mut self, obj: N::Inner) -> NodeID {
+        let node = Node::new(obj, self.id_counter);
 
         self.nodes.insert(self.id_counter, node);
         self.id_counter += 1;
@@ -51,7 +48,7 @@ impl<T> Graph<T> {
     pub fn remove_node(&mut self, id: NodeID) -> Result<(), &'static str> {
         // get edge data for this node
         let (pointing_to, pointed_to) = if let Some(node) = self.nodes.get(&id) {
-            Ok((node.pointing_to.clone(), node.pointed_to.clone()))
+            Ok((node.pointing_to().clone(), node.pointed_to().clone()))
         } else {
             Err("Invalid Node ID")
         }?;
@@ -59,13 +56,13 @@ impl<T> Graph<T> {
         // remove all edges for this node
         for other_id in pointed_to.iter() {
             if let Some(node) = self.nodes.get_mut(&other_id) {
-                (*node).pointing_to.retain(|elem| elem != &id)
+                (*node).pointing_to().retain(|elem| elem != &id)
             }
         }
 
         for other_id in pointing_to.iter() {
             if let Some(node) = self.nodes.get_mut(&other_id) {
-                (*node).pointed_to.retain(|elem| elem != &id)
+                (*node).pointed_to().retain(|elem| elem != &id)
             }
         }
 
@@ -81,13 +78,13 @@ impl<T> Graph<T> {
         self.nodes
             .get_mut(&from)
             .ok_or("Invalid Node ID for 'from' node")?
-            .pointing_to
+            .pointing_to()
             .push(to);
 
         self.nodes
             .get_mut(&to)
             .ok_or("Invalid Node ID for 'to' node")?
-            .pointed_to
+            .pointed_to()
             .push(from);
 
         Ok(())
@@ -98,20 +95,20 @@ impl<T> Graph<T> {
         self.nodes
             .get_mut(&from)
             .ok_or("Invalid Node ID for 'from' node")?
-            .pointing_to
+            .pointing_to()
             .retain(|elem| elem != &to);
 
         self.nodes
             .get_mut(&to)
             .ok_or("Invalid Node ID for 'to' node")?
-            .pointed_to
+            .pointed_to()
             .retain(|elem| elem != &to);
 
         Ok(())
     }
 }
 
-impl<T> Default for Graph<T> {
+impl<N: Node> Default for Graph<N> {
     fn default() -> Self {
         Graph {
             nodes: Default::default(),
@@ -120,28 +117,14 @@ impl<T> Default for Graph<T> {
     }
 }
 
-impl<T> std::fmt::Debug for Graph<T>
+impl<N> std::fmt::Debug for Graph<N>
 where
-    T: std::fmt::Debug,
+    N: Node + std::fmt::Debug,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Graph")
             .field("nodes", &self.nodes)
             .field("id_counter", &self.id_counter)
-            .finish()
-    }
-}
-
-impl<T> std::fmt::Debug for Node<T>
-where
-    T: std::fmt::Debug,
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Node")
-            .field("obj", &self.obj)
-            .field("id", &self.id)
-            .field("pointing_to", &self.pointing_to)
-            .field("pointed_to", &self.pointed_to)
             .finish()
     }
 }
