f191b9211d276e9dfb8202eaf4fadf1416bf8706
Who23 <contactwho23@gmail.com>
2021-01-08 21:07:50 -0500

    move util functions to seperate module

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod diff;
 pub mod graph;
 pub mod filedata;
+mod utils;
 
 use std::env;
 use std::error::Error;
@@ -30,39 +31,6 @@ fn _get_ink_dir() -> Result<PathBuf, InkError> {
     }
 }
 
-/// Find all the file paths in a directory
-fn _find_paths(dir: &Path, v: &mut Vec<PathBuf>) -> io::Result<()> {
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                _find_paths(&path, v)?;
-            } else {
-                Vec::push(v, path);
-            }
-        }
-    }
-    Ok(())
-}
-
-/// Creates a new directory at target and copies all subdirectories from source
-fn _copy_subdirs(source: &Path, target: &Path) -> Result<(), InkError> {
-    if target.is_dir() {
-        return Err("The target directory already exists".into());
-    }
-
-    let mut paths = Vec::new();
-    _find_paths(source, &mut paths)?;
-
-    for source_path in paths {
-        let source_path = source_path.strip_prefix(source).unwrap();
-        let path = target.join(source_path);
-
-        fs::create_dir_all(path.parent().unwrap())?;
-    }
-    Ok(())
-}
 
 #[derive(Debug)]
 pub enum InkError {
---
+++ src/utils.rs
@@ -0,0 +1,39 @@
+use std::fs;
+use std::path::{PathBuf, Path};
+use std::io;
+
+use crate::InkError;
+
+/// Find all the file paths in a directory
+pub fn find_paths(dir: &Path, v: &mut Vec<PathBuf>) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                find_paths(&path, v)?;
+            } else {
+                Vec::push(v, path);
+            }
+        }
+    }
+    Ok(())
+}
+
+/// Creates a new directory at target and copies all subdirectories from source
+fn _copy_subdirs(source: &Path, target: &Path) -> Result<(), InkError> {
+    if target.is_dir() {
+        return Err("The target directory already exists".into());
+    }
+
+    let mut paths = Vec::new();
+    find_paths(source, &mut paths)?;
+
+    for source_path in paths {
+        let source_path = source_path.strip_prefix(source).unwrap();
+        let path = target.join(source_path);
+
+        fs::create_dir_all(path.parent().unwrap())?;
+    }
+    Ok(())
+}
