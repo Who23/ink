713e9bf8fee20ac011551b40f7d290f132b98129
Who23 <contactwho23@gmail.com>
2021-02-07 23:48:49 -0500

    make init() function work

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,11 @@ pub mod filedata;
 pub mod graph;
 mod utils;
 
+use crate::graph::IDGraph;
+
 use std::env;
 use std::fs::{self, File};
-use std::io;
+use std::io::{self, Write};
 use std::path::PathBuf;
 
 #[macro_use]
@@ -30,12 +32,21 @@ lazy_static! {
 const DATA_EXT: &str = "data";
 const COMMIT_EXT: &str = "commit";
 
-fn _init() -> Result<(), InkError> {
+// functions called by cli
+pub fn init() -> Result<(), InkError> {
     // create ./.ink dir
     let ink_dir = env::current_dir()?.join(".ink");
-    fs::create_dir(&ink_dir)?;
 
-    File::create(&ink_dir.join("graph"))?;
+    fs::create_dir(&ink_dir)?;
+    fs::create_dir(&ink_dir.join(COMMIT_EXT))?;
+    fs::create_dir(&ink_dir.join(DATA_EXT))?;
+
+    let mut graph_file = File::create(&ink_dir.join("graph"))?;
+
+    let graph = IDGraph::new();
+    let encoded_graph = bincode::serialize(&graph)?;
+
+    graph_file.write(&encoded_graph)?;
 
     Ok(())
 }
@@ -44,6 +55,7 @@ fn _init() -> Result<(), InkError> {
 pub enum InkError {
     Err(&'static str),
     IO(io::Error),
+    Serialization(bincode::ErrorKind)
 }
 
 impl From<io::Error> for InkError {
@@ -55,5 +67,11 @@ impl From<io::Error> for InkError {
 impl From<&'static str> for InkError {
     fn from(err: &'static str) -> InkError {
         InkError::Err(err)
+    }
+}
+
+impl From<Box<bincode::ErrorKind>> for InkError {
+    fn from(err: Box<bincode::ErrorKind>) -> InkError {
+        InkError::Serialization(*err)
     }
 }
